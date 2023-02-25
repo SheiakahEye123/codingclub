f71a77d971ae2458880e0b7a9d36b8ecf3c00e5a
SheiakahEye123 <86978045+SheiakahEye123@users.noreply.github.com>
2023-02-25 08:37:03 -0800

    contours added!!

--- main.py
+++ main.py
@@ -5,13 +5,34 @@ import matplotlib.pyplot as plt
 img = cv2.imread("this.png")
 cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
 
-def findMapEdges(img):
-    grayimg = img[:,:,2]
-    thresh = cv2.adaptiveThreshold(grayimg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-    contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    blank = np.zeros_like(img, dtype=np.uint8)
-    cv2.drawContours(thresh, contours, 30, (0,0,255), 300)
-    return thresh
 
-cv2.imshow("Map shit",findMapEdges(img))
-cv2.waitKey(0)
+def findMapEdges(img):
+    grayimg = img[:, :, 2]
+    # gets grayscale img
+    thresh = ~cv2.adaptiveThreshold(grayimg, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    # adaptivethreshold finds "edges"
+    kernel = np.ones((3, 3), np.uint8)
+    erodeimg = cv2.erode(thresh, kernel)
+    gaussianimg = cv2.GaussianBlur(erodeimg, (5, 5), 0, 0)
+    # contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(gaussianimg, contours, 30, (0,0,255), 30)
+    return gaussianimg
+
+def findcs(img):
+    contours, hier = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
+    for c in contours:
+        if cv2.contourArea(c) > 2000:
+            print("c")
+            (x, y), radius = cv2.minEnclosingCircle(c)
+            center = (int(x),int(y))
+            radius = int(radius)
+            cv2.circle(img,center,radius,(255,255,255),1)
+    return img
+
+plt.imshow(findcs(findMapEdges(img)))
+plt.pause(0.01)
+
+plt.figure(2)
+plt.imshow(findMapEdges(img))
+plt.show()
