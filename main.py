3f93547b7d4d33c1a42d2eea372e763c42fa0250
SheikahEye123 <kaidenashvini@gmail.com>
2023-03-05 15:03:09 -0800

    progress ig, gonna take a break and come back wish me luck

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-import sys
 import numpy as np
 import cv2
-import matplotlib.pyplot as plt
 import pygame
 import sys
+import matplotlib.pyplot as plt
 
+sys.setrecursionlimit(5000)
 oimg = cv2.imread("this.png")
-phase = "1"
 pygame.display.init()
 screen = pygame.display.set_mode(size=(1920, 1080), vsync=240)
 
+playerpos = (0,0)
+playerposT = (int(playerpos[0]/64),int(playerpos[1]/64))
 
 class tile:
     def __init__(self, filecall, discovered, x, y):
-        self.filecall = (filecall + phase)
+        self.filecall = filecall
         self.discovered = discovered
         self.x = x
         self.y = y
 
     def draw(self):
-        tileimg = pygame.image.load(self.filecall)
-        screen.blit(tileimg, self.x, self.y)
+        if playerpos[0] - 20 <= self.x <= playerpos[0] + 20 and playerpos[1] - 20 <= self.y <= playerpos[1] + 20:
+            tileimg = pygame.transform.scale(pygame.image.load(self.filecall), (64,64))
+            screen.blit(tileimg, (self.x * 64 + playerpos[0], self.y * 64 + playerpos[1]))
 
 
 class cvImageProccesing:
     def __init__(self, img):
         self.img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        self.map = None
 
     def createMap(self):
         grayimg = self.img[:, :, 2]
@@ -39,55 +42,45 @@ class cvImageProccesing:
         for c in range(len(contours)):
             if cv2.contourArea(contours[c]) > (xs * ys) / 30:
                 cv2.drawContours(blank, contours, c, 255, 1)
-                bigc = c
                 break
-        plt.figure(1)
-        plt.imshow(blank)
-        plt.pause(0.01)
         return blank
 
     def findMap(self, blank):
-        xs, ys = blank.shape
-        map = [[]]
+        self.map = []
+        imgmap = cv2.resize(blank, (80, 106), interpolation=cv2.INTER_AREA) * 255
+        xs, ys = imgmap.shape
         for i in range(xs):
             mini = []
             for e in range(ys):
-                mini.append(tile(filecall="sea",discovered=False, x=i,y=e))
-            map.append(mini)
-        imgmap = cv2.resize(blank, (80, 106), interpolation=cv2.INTER_AREA) * 255
-
-        for x in range(len(imgmap)):
-            for y in range(len(imgmap[0])):
-                self.forFindMap(imgmap, map, x, y)
-
-        plt.figure(2)
-        plt.imshow(imgmap)
-        plt.pause(0.01)
+                mini.append(tile(filecall="sea.png", discovered=False, x=i, y=e))
+            self.map.append(mini)
+        self.forFindMap(imgmap, self.map, int(len(self.map[0])/2), int(len(self.map)/2))
 
     def forFindMap(self, imgmap, map, x, y):
-        checklist = [[x - 1, y - 1], [x, y - 1], [x + 1, y - 1], [x - 1, y], [x, y], [x + 1, y], [x - 1, y + 1],
-                     [x, y + 1], [x + 1, y + 1]]
+        checklist = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
         for i in checklist:
-            if 0 <= i[0] < len(imgmap[0]) and 0 <= i[1] < len(imgmap):
-                if i[0] != x and i[1] != y and map[i[0]][i[1]] is None:
-                    if imgmap[i[0]][i[1]] == 0:
-                        map[i[0]][i[1]] = tile(filecall="grass", discovered=True, x=i[0], y=i[1])
-                        imgmap[i[0]][i[1]] = 100
+            if 0 <= i[0] < len(map) and 0 <= i[1] < len(map[0]):
+                if imgmap[i[0]][i[1]] != 0:
+                    continue
+                map[i[0]][i[1]] = tile(filecall="grass.png", discovered=True, x=i[0], y=i[1])
+                imgmap[i[0]][i[1]] = 100
+                self.forFindMap(imgmap, map, i[0], i[1])
 
-        plt.figure(2)
+        plt.figure(0)
         plt.imshow(imgmap)
-        plt.pause(0.01)
-
+        plt.show()
         return
 
 
 imageproccesing = cvImageProccesing(oimg)
 imageproccesing.findMap(imageproccesing.createMap())
-plt.show()
 
 
 def main():
     while True:
+        for t in imageproccesing.map:
+            for te in t:
+                te.draw()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
